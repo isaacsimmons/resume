c377020df86db27ae1c62d657280f49305478626
Isaac Simmons <ids23@drexel.edu>
2015-05-27 20:37:04 -0400

    Tweak output verbosity, fail on errors

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-rm *.html
-rm *.pdf
+set -e
 
 FILENAME=${1:-resume}
+
+rm -f *.html
+rm -f *.pdf
 
 cat template/outline.1.html template/resume.css template/outline.2.html >> "$FILENAME.html"
 cat resume.md | perl scripts/markdown.pl | perl scripts/SmartyPants.pl >> "$FILENAME.html"
 cat template/outline.3.html >> "$FILENAME.html"
 
-wkhtmltopdf --page-size Letter "$FILENAME.html" "$FILENAME.pdf"
+wkhtmltopdf -q --page-size Letter "$FILENAME.html" "$FILENAME.pdf"
+
+echo "Successfully built $FILENAME.pdf"
