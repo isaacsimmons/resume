43b278cb800579e18651d2dac8118da0872ab9f6
Isaac Simmons <ids23@drexel.edu>
2015-05-27 20:23:31 -0400

    Output to letter sized paper instead of A4

--- build.sh
+++ build.sh
@@ -9,5 +9,4 @@ cat template/outline.1.html template/resume.css template/outline.2.html >> "$FIL
 cat resume.md | perl scripts/markdown.pl | perl scripts/SmartyPants.pl >> "$FILENAME.html"
 cat template/outline.3.html >> "$FILENAME.html"
 
-wkhtmltopdf "$FILENAME.html" "$FILENAME.pdf"
-#TODO: play with --page-size options in wkhtmltopdf to do pagination-friendly stuff?
+wkhtmltopdf --page-size Letter "$FILENAME.html" "$FILENAME.pdf"
