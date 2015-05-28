b8c2a09eb106d4d552cadb09e0396dca330e8562
Isaac Simmons <ids23@drexel.edu>
2015-05-27 20:08:30 -0400

    Rearrange directory structure

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 rm resume.html
 rm resume.pdf
 
-cat outline.1.html resume.css outline.2.html >> resume.html
+cat template/outline.1.html template/resume.css template/outline.2.html >> resume.html
 cat resume.md | perl scripts/markdown.pl | perl scripts/SmartyPants.pl >> resume.html
-cat outline.3.html >> resume.html
+cat template/outline.3.html >> resume.html
 
 wkhtmltopdf resume.html resume.pdf
 #TODO: play with --page-size options in wkhtmltopdf to do pagination-friendly stuff?
